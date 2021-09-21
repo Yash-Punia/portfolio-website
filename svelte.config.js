a85184d40018cad5c17e4f4cc676f5e6b07d8d2e
Zeldariomon <puniayash@gmail.com>
2021-09-21 12:16:28 +0530

    changed adapter to static

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
-import netlify from '@sveltejs/adapter-netlify'
+import adapter from '@sveltejs/adapter-static'
 import { sveltePreprocess } from 'svelte-preprocess/dist/autoProcess.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 
 const config = {
 	kit: {
-		adapter: netlify(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	},
